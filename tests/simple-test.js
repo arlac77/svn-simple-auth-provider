f64219fbe24a8406f0451997a680d1fab2715caa
Markus Felten <Markus.Felten@gmx.de>
2018-07-19 22:33:03 +0200

    test: add url to provideCredentials request

--- tests/simple-test.js
+++ tests/simple-test.js
@@ -1,11 +1,17 @@
 import test from 'ava';
 import { join } from 'path';
 import { SvnSimpleAuthProvider } from '../src/svn-simple-auth-provider';
+import { URL } from 'url';
 
 test('has values', async t => {
   const authProvider = new SvnSimpleAuthProvider();
 
-  const credentials = await authProvider.provideCredentials({ realm: 'xxx' });
+  const credentials = await authProvider.provideCredentials({
+    url: new URL(
+      'https://subversion.assembla.com/svn/delivery_notes/!svn/rvr/1487/data/environments.json'
+    ),
+    Basic: { realm: 'xxx' }
+  });
 
   t.is(credentials.user, 'xxxx');
 });
