b13dbff234d2d383ff269660e9c2c7dd7eb0de9a
Markus Felten <Markus.Felten@gmx.de>
2018-08-22 22:48:54 +0200

    fix(rollup): use rollup-plugin-cleanup

--- tests/simple-test.js
+++ tests/simple-test.js
@@ -1,34 +1,33 @@
-import test from 'ava';
-import { join } from 'path';
-import { SvnSimpleAuthProvider } from '../src/svn-simple-auth-provider';
-import { URL } from 'url';
+import test from "ava";
+import { join } from "path";
+import { SvnSimpleAuthProvider } from "../src/svn-simple-auth-provider";
 
-test('default realm Dir', t => {
+test("default realm Dir", t => {
   const authProvider = new SvnSimpleAuthProvider();
 
   t.is(
     authProvider.realmDirectory,
-    join(process.env.HOME, '.subversion', 'auth', 'svn.simple')
+    join(process.env.HOME, ".subversion", "auth", "svn.simple")
   );
 });
 
-test('given realm Dir', t => {
-  const authProvider = new SvnSimpleAuthProvider({ realmDirectory: '/tmp' });
+test("given realm Dir", t => {
+  const authProvider = new SvnSimpleAuthProvider({ realmDirectory: "/tmp" });
 
-  t.is(authProvider.realmDirectory, '/tmp');
+  t.is(authProvider.realmDirectory, "/tmp");
 });
 
-test('has values', async t => {
+test("has values", async t => {
   const authProvider = new SvnSimpleAuthProvider({
-    realmDirectory: join(__dirname, '..', 'tests', 'fixtures')
+    realmDirectory: join(__dirname, "..", "tests", "fixtures")
   });
 
   const credentials = await authProvider.provideCredentials({
     url: new URL(
-      'https://subversion.assembla.com/svn/delivery_notes/!svn/rvr/1487/data/environments.json'
+      "https://subversion.assembla.com/svn/delivery_notes/!svn/rvr/1487/data/environments.json"
     ),
-    Basic: { realm: 'Assembla Restricted Area' }
+    Basic: { realm: "Assembla Restricted Area" }
   });
 
-  t.is(credentials.user, 'arlac77');
+  t.is(credentials.user, "arlac77");
 });
